0ef40310e34c89e8249e2174ab1fa33e5995d17c
Kaique RFS <kaiquerfs53@gmail.com>
2023-06-20 17:17:46 -0300

    Correção

--- src/components/Salas.js
+++ src/components/Salas.js
@@ -5,19 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Salas extends React.Component {
   constructor(props) {
     super(props);
-    // Inicialização do estado do componente
+    // _________Inicialização do estado do componente
     this.state = {
-      salas: [], // Array vazio para armazenar os salas
-      NumSala: '', // NumSala do sala sendo adicionado ou editado
-      descrição: '', // descrição do sala sendo adicionado ou editado
-      editando: false, // Indica se o formulário está no modo de edição ou adição
-      salaEditandoId: null, // ID do sala sendo editado
-      showModal: false, // Controla a exibição do modal
+      salas: [], // _________Array vazio para armazenar os salas
+      NumSala: '', //_________ NumSala do sala sendo adicionado ou editado
+      descrição: '', // _________descrição do sala sendo adicionado ou editado
+      editando: false, // _________Indica se o formulário está no modo de edição ou adição
+      salaEditandoId: null, // _________ID do sala sendo editado
+      showModal: false, // _________Controla a exibição do modal
     };
-  } 
+  }
 
   componentDidMount() {
-    // Chamado após o componente ser montado no DOM
+    
     // Verifica se há dados de salas no localStorage e atualiza o estado
     const salasData = localStorage.getItem('salas');
 
@@ -27,15 +27,15 @@ class Salas extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Chamado sempre que o estado do componente é atualizado
-    // Salva os dados de salas no localStorage
+    // Chama sempre que o estado do componente é atualizado
+    // Salva os dados no localStorage
     if (prevState.salas !== this.state.salas) {
       localStorage.setItem('salas', JSON.stringify(this.state.salas));
     }
   }
 
   handleChange = (event) => {
-    // Manipula o evento de mudança em campos de entrada e atualiza o estado correspondente
+    // _________Manipula o evento de mudança em campos de entrada e atualiza o estado correspondente
     this.setState({ [event.target.name]: event.target.value });
   };
 
@@ -48,58 +48,49 @@ class Salas extends React.Component {
   };
 
   adicionarsala = () => {
-    // Função para adicionar ou editar um sala
+    // _________Função para adicionar ou editar um sala
     const { NumSala, descrição, editando, salaEditandoId } = this.state;
 
     if (NumSala && descrição) {
-      
-        // Verifica se o descrição é válido
-        const novosala = {
-          id: Date.now(), // Gera um ID único baseado no timestamp
-          NumSala: NumSala,
-          descrição: descrição,
-        };
 
-        let salasAtualizados = [...this.state.salas];
-        if (editando) {
-          // Se estiver no modo de edição, substitui o sala existente com o novo sala
-          const salaEditandoIndex = salasAtualizados.findIndex(sala => sala.id === salaEditandoId);
-          salasAtualizados[salaEditandoIndex] = novosala;
-        } else {
-          // Caso contrário, adiciona o novo sala ao array de salas
-          salasAtualizados.push(novosala);
-        }
+      const novosala = {
+        id: Date.now(),
+        NumSala: NumSala,
+        descrição: descrição,
+      };
 
-        // Atualiza o estado, limpando os campos e o modo de edição
-        this.setState({ salas: salasAtualizados, NumSala: '', descrição: '', editando: false, salaEditandoId: null });
-       
+      let salasAtualizados = [...this.state.salas];
+      if (editando) {
+
+        const salaEditandoIndex = salasAtualizados.findIndex(sala => sala.id === salaEditandoId);
+        salasAtualizados[salaEditandoIndex] = novosala;
+      }
+      else {
+        salasAtualizados.push(novosala);
+      }
+
+      this.setState({ salas: salasAtualizados, NumSala: '', descrição: '', editando: false, salaEditandoId: null });
+
     }
   };
 
   editarsala = (id) => {
-    // Função para entrar no modo de edição de um sala
+    // _________Função para entrar no modo de edição de um sala
     const salaEditando = this.state.salas.find(sala => sala.id === id);
     if (salaEditando) {
-      // Se o sala existe, atualiza o estado com o NumSala, descrição e informações de edição
+    // _________Se o sala existe, atualiza o estado com o NumSala, descrição e informações de edição
       this.setState({ NumSala: salaEditando.NumSala, descrição: salaEditando.descrição, editando: true, salaEditandoId: id });
 
-      // Abre o modal
+    // _________Abre o modal
       this.handleShow();
     }
   };
 
   excluirsala = (id) => {
-    // Função para excluir um sala
+    // _________Função para excluir um sala
     const novossalas = this.state.salas.filter(sala => sala.id !== id);
 
-    // Atualiza o estado
     this.setState({ salas: novossalas });
-  };
-
-  validardescrição = (descrição) => {
-    // Função para validar o formato de um descrição usando expressão regular
-    const descriçãoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return descriçãoRegex.test(descrição);
   };
 
   render() {
@@ -108,7 +99,7 @@ class Salas extends React.Component {
     return (
       <><br></br>
         <Button variant="primary" onClick={this.handleShow}>
-          Adicionar sala
+          Adicionar Sala
         </Button>
 
 
@@ -118,11 +109,11 @@ class Salas extends React.Component {
           </Modal.Header>
           <Modal.Body>
             <Form.Group controlId="NumSala">
-              <Form.Label>NumSala</Form.Label>
+              <Form.Label>N° da Sala</Form.Label>
               <Form.Control type="text" name="NumSala" value={NumSala} onChange={this.handleChange} />
             </Form.Group>
             <Form.Group controlId="descrição">
-              <Form.Label>descrição</Form.Label>
+              <Form.Label>Descrição</Form.Label>
               <Form.Control type="descrição" name="descrição" value={descrição} onChange={this.handleChange} />
             </Form.Group>
           </Modal.Body>
